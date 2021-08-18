15fbc0f99fb7e22a47144d84efee4db79f0c6bc7
Kinzamalik <kiinza.malik@gmail.com>
2021-08-18 11:22:48 +0500

    user input

---
+++ data_types.php
@@ -0,0 +1,19 @@
+<?php
+
+// string
+$name  = "kinza";
+
+// integer
+
+$number = 12345;
+
+// float
+$point_number =  12.0;
+
+// bool
+
+$is_male = "true";
+
+null;
+
+?>
---
+++ strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$pharse = "Giraffe Academy";
+
+
+echo $pharse[0] = "k";
+echo $pharse;
+// echo strtoupper($pharse);
+// echo strlen ($pharse);
+
+?>
