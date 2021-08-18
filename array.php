4a768455fcb4ba10e51d96838c785f2ac25ec0d8
Kinzamalik <kiinza.malik@gmail.com>
2021-08-18 20:25:54 +0500

    function

--- array.php
+++ array.php
@@ -1,8 +1,13 @@
 <?php
 
-$friends = array("talal","ali","rimi","daisu");
-$friends[0] = "unknown";
-echo $friends[0];
+// $friends = array("talal","ali","rimi","daisu");
+// $friends[0] = "unknown";
+// echo $friends[0];
+
+// Associative Array
+
+$friends= array("kinza" => "A+","shanza"=>"A++");
+echo $friends["shanza"]
 
 
 ?>
