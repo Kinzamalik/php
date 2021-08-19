d64e4a9b4462e2ce0a73fa3b36bd5cee55ea5b3c
Kinzamalik <kiinza.malik@gmail.com>
2021-08-19 21:11:15 +0500

    class and objects

---
+++ loops.php
@@ -0,0 +1,27 @@
+<?php
+
+$index = 1;
+
+// while($index <= 10){
+//     echo "$index <br>";
+//     $index++;
+// }
+
+
+// do{
+//     echo "$index <br>";
+//         $index++;
+// }while($index <= 10);
+
+
+// for($i = ; $i <= 5; $i++;){
+//     echo
+// }
+
+
+
+
+
+
+
+?>
